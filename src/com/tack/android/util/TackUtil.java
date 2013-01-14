a7f95c05f3f885d8475a0a66e3144e36ab598ec6
Joshua Jamison <joshua.jamison@gmail.com>
2013-01-14 09:11:22 -0500

    Adding stream to stream copy routine and BitmapHelper

--- src/com/tack/android/util/TackUtil.java
+++ src/com/tack/android/util/TackUtil.java
@@ -3,6 +3,7 @@ package com.tack.android.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -24,8 +25,27 @@ public final class TackUtil {
       }
     } catch (IOException e) {
       // Darn
+    } finally {
+      try {
+        stream.close();
+      } catch (IOException e) {
+      }
     }
     return sb.toString();
+  }
+  
+  public static void streamToStream(InputStream is, OutputStream os) throws IOException {
+    final byte[] buffer = new byte[256];
+    try {
+      int n;
+      while ((n = is.read(buffer)) != -1) {
+        os.write(buffer, 0, n);
+      }
+    } finally {
+      os.flush();
+      os.close();
+      is.close();
+    }
   }
   
   private TackUtil(){}
