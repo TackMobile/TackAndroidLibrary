b6d367da89d288a2e9e06a6facd46db80802b972
Joshua Jamison <joshua.jamison@gmail.com>
2013-02-26 14:55:12 -0700

    Adding google's bitmap caching code

---
+++ src/com/tack/android/image/BitmapHelper.java
@@ -0,0 +1,82 @@
+package com.tack.android.image;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+
+import com.tack.android.util.ConfigUtil;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public final class BitmapHelper {
+
+  private static final int THUMB_HEIGHT = 80;
+
+  private static final int MAX_SAMPLE_SIZE = 4;
+
+  public static Bitmap bitmapToThumbnail(Bitmap bitmap) {
+    final int bmpWidth = bitmap.getWidth();
+    final int bmpHeight = bitmap.getHeight();
+
+    final int ratio = THUMB_HEIGHT / bmpHeight;
+    final int newWidth = ratio * bmpWidth;
+
+    return Bitmap.createBitmap(newWidth, THUMB_HEIGHT, Bitmap.Config.ARGB_8888);
+  }
+
+  public static Bitmap scaleBytesToBitmap(byte[] imageData, int maxSize) {
+    // Decode image size
+    BitmapFactory.Options o = new BitmapFactory.Options();
+    o.inJustDecodeBounds = true;
+    BitmapFactory.decodeStream(new BufferedInputStream(new ByteArrayInputStream(imageData)), null, o);
+
+    // Find the correct scale value. It should be the power of 2.
+    int scale = 1;
+    while (o.outWidth / scale / 2 >= maxSize && o.outHeight / scale / 2 >= maxSize)
+      scale *= 2;
+
+    // Decode with inSampleSize
+    BitmapFactory.Options o2 = new BitmapFactory.Options();
+    o2.inSampleSize = scale;
+    return BitmapFactory.decodeStream(new BufferedInputStream(new ByteArrayInputStream(imageData)), null, o2);
+  }
+
+  public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    // don't scale if we don't have a size yet
+    if (reqWidth == 0 || reqHeight == 0)
+      return inSampleSize;
+
+    if (height > reqHeight || width > reqWidth) {
+      if (width > height) {
+        inSampleSize = Math.round((float) height / (float) reqHeight);
+      } else {
+        inSampleSize = Math.round((float) width / (float) reqWidth);
+      }
+    }
+    return Math.min(MAX_SAMPLE_SIZE, inSampleSize);
+  }
+
+  /**
+   * Get the size in bytes of a bitmap.
+   * 
+   * @param bitmap
+   * @return size in bytes
+   */
+  @TargetApi(12)
+  public static int getBitmapSize(Bitmap bitmap) {
+    if (ConfigUtil.hasV12()) {
+      return bitmap.getByteCount();
+    }
+    // Pre HC-MR1
+    return bitmap.getRowBytes() * bitmap.getHeight();
+  }
+
+  private BitmapHelper() {
+  }
+}
---
+++ src/com/tack/android/util/ConfigUtil.java
@@ -0,0 +1,22 @@
+package com.tack.android.util;
+
+import android.os.Build;
+
+public class ConfigUtil {
+  
+  public static boolean hasV08() {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+  }
+  
+  public static boolean hasV09() {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+  }
+
+  public static boolean hasV12() {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
+  }
+  
+  public static boolean hasV16() {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+  }
+}
