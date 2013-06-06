87e53852ba200b94f98bf3c2d050394dae2513cb
Joshua Jamison <joshua.jamison@gmail.com>
2013-06-06 16:20:40 -0600

    Updating config

--- src/com/tack/android/util/ConfigUtil.java
+++ src/com/tack/android/util/ConfigUtil.java
@@ -3,11 +3,11 @@ package com.tack.android.util;
 import android.os.Build;
 
 public class ConfigUtil {
-  
+
   public static boolean hasV08() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
   }
-  
+
   public static boolean hasV09() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
   }
@@ -15,7 +15,7 @@ public class ConfigUtil {
   public static boolean hasV11() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
   }
-  
+
   public static boolean hasV12() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
   }
@@ -23,7 +23,11 @@ public class ConfigUtil {
   public static boolean hasV13() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2;
   }
-  
+
+  public static boolean hasV14() {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+  }
+
   public static boolean hasV16() {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
   }
